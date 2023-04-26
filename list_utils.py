0735e39533c250e38c7fedda017aea9ae4cbb0a2
LeinadHunter <leinadhunter10@hotmail.com>
2023-04-26 18:32:24 +0200

    ft: Creamos nueva test y nueva funcion

--- list_utils.py
+++ list_utils.py
@@ -12,5 +12,33 @@ def find_one(list, needle):
             found = True
         # avanzamos al siguiente elemento
         index = index + 1
+
     # devolvemos si hemos encontrado o no
     return found
+
+
+def find_n(list, needle, n):
+    """
+    Devuelve True si en list hay n o más ocurrencias de needle
+    False is hay menos o si n < 0
+    """
+    # si n > 0...
+    if n > 0:
+        # Inicializamos el índice y el contador
+        index = 0
+        count = 0
+
+        # mientras no hayamos encontrado a lelemento n veces o no hayamos terminado la lista...
+        while count < n and index < len(list):
+            # si lo encontramos, actualizamos el contador
+            if needle == list[index]:
+                count = count + 1
+
+            # avanzamos al siguiente elemento
+            index = index + 1
+
+        # devolvemos el resultado de comparar el contador con n
+        return count >= n
+
+    else:
+        return False
--- list_utils_test.py
+++ list_utils_test.py
@@ -14,3 +14,11 @@ def test_find_one():
     assert find_one(beginning, needle)
     assert find_one(end, needle)
     assert find_one(several, needle)
+
+
+def test_find_n():
+    assert find_n([2, 3, 4, 5, 6], 2, -1) == False
+    assert find_n([1, 2, 3, 4, 5], 42, 2) == False
+    assert find_n([1, 2, 3, 4, 5], 1, 2) == False
+    assert find_n([1, 2, 3, 2, 4, 5], 2, 2)
+    assert find_n([1, 2, 3, 4, 5, 4, 6, 4, 7, 4, 6], 4, 2)
