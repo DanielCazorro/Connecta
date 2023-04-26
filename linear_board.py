59c44a50142a64e84f943c783a2c45e3dd05bddc
LeinadHunter <leinadhunter10@hotmail.com>
2023-04-26 13:06:46 +0200

    ft: Creamos diferentes clases para los test

--- linear_board.py
+++ linear_board.py
@@ -1,2 +1,34 @@
+from settings import BOARD_LENGTH
+
+
 class LinearBoard():
-    pass
+    """
+    Clase que representa un tablero de una sola columna
+    x un jugador
+    o otro jugador
+    None un espacio vacío
+    """
+
+    def __init__(self):
+        """
+        Una lista de None
+        """
+        self._colum = [None for i in range(BOARD_LENGTH)]
+
+    def add(self, char):
+        """
+        Juega en la primera posición disponible
+        """
+        # buscamos la priemra posición libre (None)
+        i = self._colum.index(None)
+        # lo sustituimos por un char
+        self._colum[i] = char
+
+    def is_full(self):
+        pass
+
+    def is_victory(self, char):
+        return False
+
+    def is_tie(self, char1, char2):
+        return False
