4c4bbcf1742c2088e1addf5de130a2f5792228e4
LeinadHunter <leinadhunter10@hotmail.com>
2023-04-27 12:03:47 +0200

    ft: Añadimos función streak

--- list_utils.py
+++ list_utils.py
@@ -2,19 +2,7 @@ def find_one(list, needle):
     """
     Devuelve True si encuentar una o más ocurencias de needle en list
     """
-    # inicializamos el bool que representa la condicion de haber encontrado o no el indice
-    found = False
-    index = 0
-    # mientras no encontramos o hayamos terminado con la lista
-    while not found and index < len(list):
-        # miramos a ver si está en la posición actual y actualizamos la condición
-        if needle == list[index]:
-            found = True
-        # avanzamos al siguiente elemento
-        index = index + 1
-
-    # devolvemos si hemos encontrado o no
-    return found
+    return find_n(list, needle, 1)
 
 
 def find_n(list, needle, n):
@@ -41,4 +29,37 @@ def find_n(list, needle, n):
         return count >= n
 
     else:
+        return False
+
+
+def find_streak(list, needle, n):
+    """
+    Devuelve True si en list hay n o más needles seguidos
+    False, para todo lo demás
+    """
+    # Si n>=0
+    if n >= 0:
+        # Inicializo el índice, el contador y el indicador de racha
+        index = 0
+        count = 0
+        streak = False
+
+        # Mientras no haya encontrado a n sguidos y la lista no se haya acabado...
+        while count < n and index < len(list):
+            # Si lo encuentro, activo el indicador de rachas y actualizao el contador
+            if needle == list[index]:
+                streak = True
+                count = count + 1
+            else:
+                # Si no lo encuentro, desactivo el indicador de la racha y pongo a cero el contador
+                streak = False
+                count = 0
+
+            # Avanzo al siguiente elemento
+            index = index + 1
+
+        # Devolvemos el resultado de comparar el contador con n SIEMPRE Y CUANDO estemos en racha
+        return count >= n and streak
+    else:
+        # Para valores de n < 0, no tiene sentido
         return False
