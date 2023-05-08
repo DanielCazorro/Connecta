6f34c80ec914b7140c0f6fb234acaf1b66ec6443
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-05-08 17:07:02 +0200

    ft: Creamos test is wining

--- oracle_test.py
+++ oracle_test.py
@@ -1,4 +1,6 @@
 from oracle import *
+from player import Player
+from settings import BOARD_LENGTH
 from square_board import SquareBoard
 
 
@@ -28,3 +30,25 @@ def test_equality():
     # no equivalentes (puesto qu eno tienen la misma clasificación)
     assert cr != ColumnRecommendation(2, ColumnClassification.FULL)
     assert cr != ColumnRecommendation(3, ColumnClassification.FULL)
+
+def test_is_winning_move():
+    winner = Player('Xavier', 'x')
+    loser = Player('Otto', 'o')
+
+    empty = SquareBoard()
+    almost = SquareBoard.fromList([['o', 'x', 'o', None],
+                                   ['o', 'x', 'o', None],
+                                   ['x', None, None, None],
+                                   [None, None, None, None]])
+    oracle = SmartOracle()
+
+    # sobre tablero vacío
+    for i in range(0, BOARD_LENGTH):
+        assert oracle._is_winning_move(empty, i, winner) == False
+        assert oracle._is_winning_move(empty, i, loser) == False
+
+    # sobre el tablero de verdad
+    for i in range(0, BOARD_LENGTH):
+        assert oracle._is_winning_move(almost, i, loser) == False
+
+    assert oracle._is_winning_move(almost, 2, winner)
